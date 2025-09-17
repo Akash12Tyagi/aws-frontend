965d2a13ed893957ac2480f93fcc3c5424bb9eaf
sandeep.singh <sandeep.singh@brightzone.us>
2025-09-17 09:20:56 +0530

    initial commit | Sandeep Singh

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import HealthCheck from "./components/HealthCheck";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 API Health Dashboard</h1>
+      <HealthCheck />
+    </div>
+  );
+}
+
+export default App;
